0459fabe6251aed1449e178fddb1b07d9d4c73e0
Peter <thong01677@gmail.com>
2021-07-17 21:40:00 -0400

    adding state to project and fetching data from api

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { GlobalStyle } from "./GlobalStyle";
 
 //Components
 import Header from "./components/Header";
+import Home from "./components/home";
+
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Home />
       <GlobalStyle />
     </div>
   );
