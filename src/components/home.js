68a7a26cf043ed4442d9907b1e0f3b2d07791b00
Peter <thong01677@gmail.com>
2021-07-19 18:22:19 -0400

    create the search term sync with state

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
@@ -11,6 +11,10 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 
 import Thumb from "./Thumb";
+
+import Spinner from "./Spinner";
+
+import SearchBar from "./SearchBar";
 //Hook
 
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -20,9 +24,7 @@ import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImg from "../images/no_image.jpg";
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
-  console.log(state);
-
+  const { state, loading, error, setSearchTerm } = useHomeFetch();
   return (
     <>
       {state.results[0] ? (
@@ -32,6 +34,8 @@ const Home = () => {
           text={state.results[0].overview}
         />
       ) : null}
+
+      <SearchBar setSearchTerm={setSearchTerm} />
 
       <Grid header="Popular Movies">
         {state.results.map((movie) => (
@@ -47,6 +51,8 @@ const Home = () => {
           />
         ))}
       </Grid>
+
+      <Spinner />
     </>
   );
 };
