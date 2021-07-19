9834078fb2c474ac257ddb227ec09624d038aac4
Peter <thong01677@gmail.com>
2021-07-19 16:40:43 -0400

    create a custom hook to wrap the logic of fetching data

--- src/components/home.js
+++ src/components/home.js
@@ -6,44 +6,20 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import API from "../API";
 
 //Components
-
+import HeroImage from "./HeroImage";
 //Hook
+
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 //Image
 
 import NoImg from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false);
-      setLoading(true);
-      const movies = await API.fetchMovies(searchTerm, page);
-      console.log(movies);
-      setState((prev) => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
-      }));
-    } catch (error) {
-      setError(true);
-    }
-
-    setLoading(false);
-  };
-
-  //initial render
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
-
+  const { state, loading, error } = useHomeFetch();
   console.log(state);
 
-  return <div>Home page</div>;
+  return <HeroImage />;
 };
 
 export default Home;
