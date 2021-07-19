3b322719ebcc012edb57b5699ebd1e2d335c687b
Peter <thong01677@gmail.com>
2021-07-19 17:29:33 -0400

    created the header of the app

--- src/components/home.js
+++ src/components/home.js
@@ -7,6 +7,8 @@ import API from "../API";
 
 //Components
 import HeroImage from "./HeroImage";
+
+import Grid from "./Grid";
 //Hook
 
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -19,7 +21,23 @@ const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state);
 
-  return <HeroImage />;
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default Home;
