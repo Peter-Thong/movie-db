e0c64871c87bff5d6fb60246d9ca40a844e25c43
Peter <thong01677@gmail.com>
2021-07-20 23:38:39 -0400

    complete the header of movie page to show the info of movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,38 @@
 import React from "react";
 
-const Movie = () => <div>Movie</div>;
+import { useParams } from "react-router-dom";
+
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+
+import Grid from "./Grid";
+
+import Spinner from "./Spinner";
+
+import BreadCrumb from "./BreadCrumb";
+
+import MovieInfo from "./MovieInfo";
+
+//Hook
+
+import { useMovieFetch } from "../hooks/useMovieFetch";
+
+//image
+
+import NoImg from "../images/no_image.jpg";
+
+const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+  console.log(movieId);
+
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong</div>;
+  return (
+    <>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+    </>
+  );
+};
 
 export default Movie;
